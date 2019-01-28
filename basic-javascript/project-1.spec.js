77534926a41b00c3d8fab3e70b0d672ba7f4b525
Nicholas O'Ferrall <nickoferrall@gmail.com>
2019-01-28 16:29:34 -0500

    First few test scripts complete

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -12,8 +12,41 @@ it('Should run tests using it instead of test.', () => {
 
 describe('multiplyByTen', () => {
     it('Should multiply provided number by ten', () => {
-        const expected = 101;
+        const expected = 100;
         const actual = helpers.multiplyByTen(10);
         expect(actual).toEqual(expected);
     })
+})
+
+it('Should subtract by 5', () => {
+    const expected = 5;
+    const actual = helpers.subtractFive(10);
+    expect(actual).toEqual(expected);
+})
+
+it('Should check if they are the same length', () => {
+    const str1 = 'Tom';
+    const str2 = 'Ben';
+    console.log(areSameLength(str1, str2))
+    expect(str1.length).toEqual(str2.length);
+})
+
+// it('Should check if they are the same', () => {
+//     const str1 = 'Tom';
+//     const str2 = 'Tom';
+//     expect(str1).toBe(str2);
+// })
+
+
+// it('Should check if number is less than 90', () => {
+//     const num = 85;
+//     expect(num).toBeLessThan(90);
+// })
+
+it('Should add the two inputs', () => {
+    const num1 = 10;
+    const num2 = 5;
+    expected = 15;
+    actual = add(num1, num2);
+    expect(actual).toEqual(expected);
 })
