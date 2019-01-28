7769313866e1f3b3e4f7b2c71f346cbc0cd1876a
Nicholas O'Ferrall <nickoferrall@gmail.com>
2019-01-28 15:38:00 -0500

    Initial commit with test

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,19 @@
 const helpers = require('./project-1');
 
 // start testing!
+
+test('Should run test', () => {
+
+})
+
+it('Should run tests using it instead of test.', () => {
+
+})
+
+describe('multiplyByTen', () => {
+    it('Should multiply provided number by ten', () => {
+        const expected = 101;
+        const actual = helpers.multiplyByTen(10);
+        expect(actual).toEqual(expected);
+    })
+})
